33da44a5a3ecf3e79d08dd51c7f66cac8be1a7b0
Yevhenii Sosidko <40215123+NoCodingNoLifee@users.noreply.github.com>
2024-09-27 15:41:50 +0300

    test with publicUrl from config

--- src/sxastarter/src/components/RichText.tsx
+++ src/sxastarter/src/components/RichText.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Field, RichText as JssRichText } from '@sitecore-jss/sitecore-jss-nextjs';
 import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 
+import config from 'temp/config';
+
+
+
 interface Fields {
   Text: Field<string>;
 }
@@ -18,20 +22,16 @@ export const Default = (props: RichTextProps): JSX.Element => {
     <span className="is-empty-hint">Rich text</span>
   );
   const id = props.params.RenderingIdentifier;
+  const publicUrl = config.publicUrl;
 
-  const resp = fetch(getPublicUrl() + "/api/custom/test");
-  const resp2 = fetch("/api/custom/test");
-  console.log(getPublicUrl());
+  const resp = fetch(publicUrl + "/api/custom/test");
+  //const resp2 = fetch("/api/custom/test");
+  console.log(publicUrl);
 
   resp.then(intResp =>{
     //const text = resp2.text;
-    console.log("Request with getPublicUrl method:");
+    console.log("Request with publicUrl method:");
     console.log(intResp);
-  });
-
-  resp2.then(intResp2 =>{
-    console.log("Request without getPublicUrl method:");
-    console.log(intResp2);
   });
 
   return (
